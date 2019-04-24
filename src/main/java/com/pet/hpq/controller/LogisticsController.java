2a36cd6bf1eb870ad0e5719b62f168dbe7b12a21
ppamos <503226986@qq.com>
2019-04-24 14:28:36 +0800

    何培强的上传

---
+++ src/main/java/com/pet/hpq/controller/LogisticsController.java
@@ -0,0 +1,38 @@
+package com.pet.hpq.controller;
+
+import com.pet.hpq.service.OrderService;
+import com.pet.hpq.service.ReturnSale;
+import com.pet.hpq.vo.OrderInfoVo;
+import com.pet.hpq.vo.ReturnMessageVo;
+import com.pet.tools.SplitPage;
+import com.pet.yh.pojo.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+@RestController
+public class LogisticsController {
+    @Autowired(required = false)
+    ReturnSale returnSale;
+
+    @RequestMapping(value = "getReturnMessage",method = RequestMethod.POST)
+    public Object getReturnMessage(@RequestBody(required = false)ReturnMessageVo returnMessageVo, HttpSession httpSession){
+//        Customer customer1 = new Customer();
+//        customer1.setCustomerId(2);
+//        httpSession.setAttribute("customer",customer1);
+        Customer customer = (Customer)httpSession.getAttribute("customer");
+        returnMessageVo.setCustomerId(customer.getCustomerId());
+        int i = returnSale.addReturnSale(returnMessageVo);
+        return i;
+    }
+    @RequestMapping(value = "getReturnDetail",method = RequestMethod.POST)
+    public Object getReturnDetail(@RequestBody(required = false)ReturnMessageVo returnMessageVo, HttpSession httpSession){
+
+        return returnSale.getReturnDetail(returnMessageVo);
+    }
+
+}
---
+++ src/main/java/com/pet/hpq/service/impl/ReturnSaleImpl.java
@@ -0,0 +1,58 @@
+package com.pet.hpq.service.impl;
+
+import com.pet.hpq.dto.ReturnDetailDto;
+import com.pet.hpq.dto.StatusDto;
+import com.pet.hpq.mapper.SalesReturnMapper;
+import com.pet.hpq.mapper.TOrderMapper;
+import com.pet.hpq.service.ReturnSale;
+import com.pet.hpq.vo.ChangeGoodsStatusVo;
+import com.pet.hpq.vo.ReturnMessageVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Service
+public class ReturnSaleImpl implements ReturnSale {
+
+    @Autowired(required = false)
+    SalesReturnMapper salesReturnMapper;
+    @Autowired(required = false)
+    TOrderMapper tOrderMapper;
+
+    @Transactional
+    public int addReturnSale(ReturnMessageVo returnMessageVo) {
+        int returnStatus = returnMessageVo.getStatus();
+        if (returnStatus==1||returnStatus==2){
+            StatusDto statusDto = tOrderMapper.checkStatus(returnMessageVo);
+            int orderStatus = statusDto.gettOderStatus();
+            int goodsStatus = statusDto.getGoodsStatus();
+            System.out.println(orderStatus);
+            System.out.println(goodsStatus);
+            if (goodsStatus!=1&&goodsStatus!=2) {
+                if (orderStatus == 1) {
+                    return 0;
+                }
+                if ((orderStatus == 2 || orderStatus == 3 && returnStatus == 1)||(orderStatus == 4 || orderStatus == 5 || orderStatus == 6 && returnStatus == 2)) {
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMwwDDkkmmss");
+                    Date date =new Date();
+                    String format = dateFormat.format(date);
+                    format+=returnMessageVo.getOgId();
+                    returnMessageVo.setReturnNumber(format);
+                    salesReturnMapper.insertSelective(returnMessageVo);
+                    ChangeGoodsStatusVo changeGoodsStatusVo = new ChangeGoodsStatusVo();
+                    changeGoodsStatusVo.setOgId(returnMessageVo.getOgId());
+                    changeGoodsStatusVo.setStatus(1);
+                    return  tOrderMapper.setStatus(changeGoodsStatusVo);
+                }
+            }
+        }
+        return 0;
+    }
+
+    public ReturnDetailDto getReturnDetail(ReturnMessageVo returnMessageVo) {
+        return salesReturnMapper.getReturnDetail(returnMessageVo);
+    }
+}
