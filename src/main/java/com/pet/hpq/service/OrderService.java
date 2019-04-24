4ca1005444770e164377810570d7b3ffc56b11b4
ppamos <503226986@qq.com>
2019-04-24 11:00:22 +0800

    何培强的上传

--- src/main/java/com/pet/hpq/service/OrderService.java
+++ src/main/java/com/pet/hpq/service/OrderService.java
@@ -1,12 +1,13 @@
 package com.pet.hpq.service;
 
+import com.pet.hpq.dto.GoodsDto;
+import com.pet.hpq.dto.OrderDetail;
 import com.pet.hpq.dto.OrderPageDto;
 import com.pet.hpq.pojo.TOrder;
+import com.pet.hpq.vo.GoodsOrderVo;
 import com.pet.hpq.vo.OrderInfoVo;
-import com.pet.yh.pojo.Customer;
+import com.pet.hpq.vo.ReturnMessageVo;
 
-
-import java.util.List;
 
 public interface OrderService {
     OrderPageDto getOrders(OrderInfoVo orderInfoVo);
@@ -14,4 +15,9 @@ public interface OrderService {
     int getOrderCount(int CustomerId);
     int delivery(TOrder tOrder);
     int refuseOrder(TOrder tOrder);
+    OrderDetail getOrderDetail(TOrder tOrder);
+    int checkedDeliver(TOrder tOrder);
+    GoodsDto getGoodsInfo(GoodsOrderVo goodsOrderVo);
+    GoodsDto getRefuseGoods(GoodsOrderVo goodsOrderVo);
+    int setStatus(ReturnMessageVo returnMessageVo);
 }
--- src/main/java/com/pet/hpq/service/impl/OrderServiceImpl.java
+++ src/main/java/com/pet/hpq/service/impl/OrderServiceImpl.java
@@ -1,12 +1,12 @@
 package com.pet.hpq.service.impl;
 
-import com.pet.hpq.dto.GoodsInfoDto;
-import com.pet.hpq.dto.OrderPageDto;
-import com.pet.hpq.dto.TOrderDto;
+import com.pet.hpq.dto.*;
 import com.pet.hpq.mapper.TOrderMapper;
 import com.pet.hpq.pojo.TOrder;
 import com.pet.hpq.service.OrderService;
+import com.pet.hpq.vo.GoodsOrderVo;
 import com.pet.hpq.vo.OrderInfoVo;
+import com.pet.hpq.vo.ReturnMessageVo;
 import com.pet.tools.SplitPage;
 import com.pet.yh.pojo.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -52,10 +53,41 @@ public class OrderServiceImpl implements OrderService {
 
     public int refuseOrder(TOrder tOrder) {
         int status = tOrderMapper.getStatus(tOrder.getOrderNumber());
-        System.out.println(status);
         if (status==1||status==2){
            return tOrderMapper.refuseOrder(tOrder);
         }
+        return 0;
+    }
+
+    public OrderDetail getOrderDetail(TOrder tOrder) {
+        return tOrderMapper.getOrderDetail(tOrder);
+    }
+
+    public int checkedDeliver(TOrder tOrder) {
+        TOrder nTOrder = tOrderMapper.getCreatTime(tOrder);
+        if (nTOrder==null){
+            return 0;
+        }
+        long creatTime = nTOrder.getCreateTime().getTime();
+        int status = nTOrder.getStatus();
+        long nowTime = new Date().getTime();
+        if ((nowTime-creatTime)>1296000000||status==1){
+            return 0;
+        }
+        return 1;
+    }
+
+    public GoodsDto getGoodsInfo(GoodsOrderVo goodsOrderVo) {
+        GoodsDto goodsInfoDto = tOrderMapper.getGoodsInfo(goodsOrderVo);
+        return goodsInfoDto;
+    }
+
+    public GoodsDto getRefuseGoods(GoodsOrderVo goodsOrderVo) {
+        GoodsDto refuseGoods = tOrderMapper.getRefuseGoods(goodsOrderVo);
+        return refuseGoods;
+    }
+
+    public int setStatus(ReturnMessageVo returnMessageVo) {
         return 0;
     }
 }
