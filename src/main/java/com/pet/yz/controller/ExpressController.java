a3413d37b46d513ada45e697d3f13d628db38661
ZzhangSamA <543606197@qq.com>
2019-04-26 11:15:21 +0800

    快递鸟接入

--- src/main/java/com/pet/yz/controller/ExpressController.java
+++ src/main/java/com/pet/yz/controller/ExpressController.java
@@ -1,5 +1,7 @@
 package com.pet.yz.controller;
 
+import com.pet.hpq.pojo.TOrder;
+import com.pet.hpq.service.OrderService;
 import com.pet.yz.service.ExpressService;
 import com.pet.yz.vo.ExpressVo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,11 +17,24 @@ public class ExpressController {
     @Autowired
     ExpressService expressService;
 
+    @Autowired
+    OrderService orderService;
+
     @ResponseBody
     @RequestMapping(value = "getExpressInformation",method = RequestMethod.POST)
     public void getExpressInformation(@RequestBody(required = false) ExpressVo expressVo) throws Exception {
         String expressInformation = expressService.getExpressInformation(expressVo);
         System.out.println(expressInformation);
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "getOrderForExpress",method = RequestMethod.POST)
+    public Object getOrderForExpress(@RequestBody(required = false)TOrder tOrder) {
+        TOrder order = orderService.getOrderByOrderNumber(tOrder.getOrderNumber());
+        if (order == null) {
+            return false + "";
+        }
+        return order;
     }
 
 }
